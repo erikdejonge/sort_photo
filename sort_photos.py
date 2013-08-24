b8d50ac0ae7506e979ad84d499e651fa7a9894a3
Erik de Jonge <erik@a8.nl>
2013-08-24 05:51:45 +0200

    -

--- sort_photos.py
+++ sort_photos.py
@@ -46,6 +46,7 @@ def valid_types(filepath):
         "gif",
         "3gp",
         "mp4",
+        "mts",
         "mpg",
         "cr2",
         "bmp",
@@ -175,6 +176,7 @@ def ensure_directory(year, month, day):
     if not os.path.exists(month_path):
         os.mkdir(month_path)
     if not os.path.exists(day_path):
+        print day_path
         os.mkdir(day_path)
     return day_path
 
@@ -209,8 +211,8 @@ def main():
             if not os.path.exists(day_path + "/" + os.path.basename(filepath)):
                 print filepath
                 #print "moved:", day_path + "/" + os.path.basename(filepath)
-                #print ('mv "' + shell_escape(filepath) + '" ' + shell_escape(day_path) + "/")
-                os.system('mv ' + shell_escape(filepath) + ' ' + shell_escape(day_path) + "/")
+
+                os.system('cp  ' + shell_escape(filepath) + ' ' + shell_escape(day_path) + "/")
                 pics_moved += 1
             else:
                 print day_path + "/" + os.path.basename(filepath), "exists"
@@ -221,4 +223,5 @@ def main():
 
 if __name__ == "__main__":
     while main() != 0:
+        exit(1)
         pass
