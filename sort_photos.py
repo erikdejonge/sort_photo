0908750e0b6269b1fa9161467e2e97dd7cbce4b5
Erik de Jonge <erik@a8.nl>
2016-05-03 17:24:32 +0200

    rabshakeh:Tuesday 03 May 2016 (week:18 day:124), 17:24:25

--- sort_photos.py
+++ sort_photos.py
@@ -23,8 +23,8 @@ from dateutil.parser import parse
 
 SOURCEDIR = "/Users/rabshakeh/Dropbox (Personal)/Camera Uploads"
 #SOURCEDIR= "/Users/rabshakeh/camera_uploads_aug_2015"
-TARGETDIR = "/Users/rabshakeh/Dropbox (Active8)/photos"
-#TARGETDIR = "/Users/rabshakeh/fulldropbox/photos"
+#TARGETDIR = "/Users/rabshakeh/Dropbox (Active8)/photos"
+TARGETDIR = "/Users/rabshakeh/fulldropbox/photos"
 #TARGETDIR = "/Volumes/bu/sldropbox/photos"
 #TARGETDIR = "/Volumes/bu/sldropbox/photos"
 
