20da6052e2bceece18130173d4ce90ebd98e7ba4
Erik de Jonge <erik@a8.nl>
2016-09-29 11:33:45 +0200

    Thursday 29 September 2016 (week:39 day:273), 11:33:41

--- sort_a8.py
+++ sort_a8.py
@@ -2,6 +2,7 @@ import sort_photos
 
 sort_photos.SOURCEDIR = "/Users/rabshakeh/Dropbox (Personal)/Camera Uploads2"
 sort_photos.TARGETDIR = "/Users/rabshakeh/dropboxa8/photos"
+import os
 
 
 if __name__ == "__main__":
