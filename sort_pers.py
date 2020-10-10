d1b95504bea2693c0ba69f8d629ed278f7ee6eba
Erik de Jonge <erik@a8.nl>
2020-10-10 18:32:37 +0200

    Saturday 10 October 2020 (week:40 day:284), 18:32:33

--- sort_pers.py
+++ sort_pers.py
@@ -1,6 +1,6 @@
 import sort_photos
 
-sort_photos.SOURCEDIR = "/Volumes/2tb/Dropbox/Dropbox (Personal)/Camera Uploads"
+sort_photos.SOURCEDIR = "/Volumes/2tb/Dropbox/Dropbox (Personal)/Camerauploads"
 sort_photos.TARGETDIR = "/Volumes/2tb/Dropbox/Dropbox (Active8)/photos"
 
 
